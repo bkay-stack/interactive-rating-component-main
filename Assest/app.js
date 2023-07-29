7aaa58df316694e0feeddbf04a7263fc919e8a32
bkay-stack <kevinbest1900@gmail.com>
2023-07-29 08:53:46 +0100

    New codes

--- Assest/app.js
+++ Assest/app.js
@@ -1,52 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     const submitBtn = document.querySelector('.submit-btn');
-    const btn = document.querySelectorAll('.btns');
-    const ratePoint = document.getElementById('ratings');
-    const rate = document.getElementById('rate');
+    const rating = document.querySelectorAll('.btns');
+    const rates = document.getElementById('rate');
+    
 
 
-let selectedRate = '';
-let val = null
-
-console.log(val)
-
-// Event listeners 
-
-submitBtn.addEventListener('click', () => {
-    // console.log('clicked')
-    if (selectedRate === '') {
-        alert(`Please select a rating`);
-        window.location = '';
-    }else {
-        alert(`You selected ${selectedRate}`);
-        window.location.href = '/Html/index.html';
-    }   
-})
-
-
-
-btn.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        // console.log('clicked');
-        btn.classList.toggle('inactive');
-        selectedRate = btn.innerHTML;
-        console.log(selectedRate)
-        updateSelectedRating();
+    // Event listeners 
+    submitBtn.addEventListener('click', () => {
+        // console.log('clicked')
+        window.location.href = '/Html/index.html';  
     });
 
-});
-
-
-
-// Function to update the displayed rating in the span
-const updateSelectedRating = () => {
-       
-   if (rate === '' || rate === 0) {
-    rate.style.display = 'block';
-    rate.textContext = selectedRate;
-   }
-       
-        
-}
-
+    rating.forEach((rating) => {
+        rating.addEventListener('click', () => {
+            console.log(rating.innerHTML);
+            // rates.innerHTML = rating.innerHTML;
+          
+        });
+    });
 });
